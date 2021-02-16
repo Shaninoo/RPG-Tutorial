3ccc1465791291c2b33bce978c4c7bf935b39eaf
Shaninoo <chrisgconnors@gmail.com>
2021-02-16 19:27:08 +1100

    Add empty project

--- RPG Project/Assets/Scripts/EquipmentManager.cs
+++ RPG Project/Assets/Scripts/EquipmentManager.cs
@@ -8,13 +8,7 @@ public class EquipmentManager : MonoBehaviour {
 
 	#region Singleton
 
-    public enum MeshBlendShape {Torso, Arms, Legs };
-    public Equipment[] defaultEquipment;
-
 	public static EquipmentManager instance;
-	public SkinnedMeshRenderer targetMesh;
-
-    SkinnedMeshRenderer[] currentMeshes;
 
 	void Awake ()
 	{
@@ -28,7 +22,6 @@ public class EquipmentManager : MonoBehaviour {
 	// Callback for when an item is equipped/unequipped
 	public delegate void OnEquipmentChanged(Equipment newItem, Equipment oldItem);
 	public OnEquipmentChanged onEquipmentChanged;
-   
 
 	Inventory inventory;	// Reference to our inventory
 
@@ -39,9 +32,6 @@ public class EquipmentManager : MonoBehaviour {
 		// Initialize currentEquipment based on number of equipment slots
 		int numSlots = System.Enum.GetNames(typeof(EquipmentSlot)).Length;
 		currentEquipment = new Equipment[numSlots];
-        currentMeshes = new SkinnedMeshRenderer[numSlots];
-
-        EquipDefaults();
 	}
 
 	// Equip a new item
@@ -50,7 +40,15 @@ public class EquipmentManager : MonoBehaviour {
 		// Find out what slot the item fits in
 		int slotIndex = (int)newItem.equipSlot;
 
-        Equipment oldItem = Unequip(slotIndex);
+		Equipment oldItem = null;
+
+		// If there was already an item in the slot
+		// make sure to put it back in the inventory
+		if (currentEquipment[slotIndex] != null)
+		{
+			oldItem = currentEquipment[slotIndex];
+			inventory.Add(oldItem);
+		}
 
 		// An item has been equipped so we trigger the callback
 		if (onEquipmentChanged != null)
@@ -60,26 +58,17 @@ public class EquipmentManager : MonoBehaviour {
 
 		// Insert the item into the slot
 		currentEquipment[slotIndex] = newItem;
-        AttachToMesh(newItem, slotIndex);
 	}
 
 	// Unequip an item with a particular index
-	public Equipment Unequip (int slotIndex)
+	public void Unequip (int slotIndex)
 	{
-        Equipment oldItem = null;
 		// Only do this if an item is there
 		if (currentEquipment[slotIndex] != null)
 		{
 			// Add the item to the inventory
-			oldItem = currentEquipment[slotIndex];
+			Equipment oldItem = currentEquipment[slotIndex];
 			inventory.Add(oldItem);
-
-            SetBlendShapeWeight(oldItem, 0);
-            // Destroy the mesh
-            if (currentMeshes[slotIndex] != null)
-            {
-                Destroy(currentMeshes[slotIndex].gameObject);
-            }
 
 			// Remove the item from the equipment array
 			currentEquipment[slotIndex] = null;
@@ -90,7 +79,6 @@ public class EquipmentManager : MonoBehaviour {
 				onEquipmentChanged.Invoke(null, oldItem);
 			}
 		}
-        return oldItem;
 	}
 
 	// Unequip all items
@@ -100,42 +88,7 @@ public class EquipmentManager : MonoBehaviour {
 		{
 			Unequip(i);
 		}
-
-        EquipDefaults();
 	}
-
-    void AttachToMesh(Equipment item, int slotIndex)
-	{
-
-        SkinnedMeshRenderer newMesh = Instantiate(item.mesh) as SkinnedMeshRenderer;
-        newMesh.transform.parent = targetMesh.transform.parent;
-
-        newMesh.rootBone = targetMesh.rootBone;
-		newMesh.bones = targetMesh.bones;
-		
-		currentMeshes[slotIndex] = newMesh;
-
-
-        SetBlendShapeWeight(item, 100);
-       
-	}
-
-    void SetBlendShapeWeight(Equipment item, int weight)
-    {
-		foreach (MeshBlendShape blendshape in item.coveredMeshRegions)
-		{
-			int shapeIndex = (int)blendshape;
-            targetMesh.SetBlendShapeWeight(shapeIndex, weight);
-		}
-    }
-
-    void EquipDefaults()
-    {
-		foreach (Equipment e in defaultEquipment)
-		{
-			Equip(e);
-		}
-    }
 
 	void Update ()
 	{
--- RPG Project/Assets/Scripts/Items/Equipment.cs
+++ RPG Project/Assets/Scripts/Items/Equipment.cs
@@ -7,14 +7,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
 public class Equipment : Item {
 
-
-
 	public EquipmentSlot equipSlot;	// Slot to store equipment in
 
 	public int armorModifier;		// Increase/decrease in armor
 	public int damageModifier;      // Increase/decrease in damage
-    public SkinnedMeshRenderer mesh;
-    public EquipmentManager.MeshBlendShape[] coveredMeshRegions;
 
 	// When pressed in inventory
 	public override void Use()
